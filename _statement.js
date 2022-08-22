34d39d9d9fb0c7395d8f568ed74f2217e41b7838
Elyas Hesari <elyashesari@gmail.com>
2022-08-22 18:44:23 +0430

    DataType in JavaScript

--- _statement.js
+++ _statement.js
@@ -1,5 +1,5 @@
-document.getElementById("demo").innerHTML = "Function getElementByID";
-document.write("function document.write");
-// window.alert("funcotion window alert");
-// alert("function alert");
-console.log("My text for console");
+// document.getElementById("demo").innerHTML = "Function getElementByID";
+// document.write("function document.write");
+// // window.alert("funcotion window alert");
+// // alert("function alert");
+// console.log("My text for console");
