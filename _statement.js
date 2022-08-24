a659fa71446af6557f8f21598c27fd14c8f011d2
Elyas Hesari <elyashesari@gmail.com>
2022-08-24 10:58:15 +0430

    Type conversation for convert string to number

--- _statement.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById("demo").innerHTML = "Function getElementByID";
-// document.write("function document.write");
-// // window.alert("funcotion window alert");
-// // alert("function alert");
-// console.log("My text for console");
