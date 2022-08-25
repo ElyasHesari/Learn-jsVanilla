ad43e3d808748810012ad1012be0ca0cd6a60390
Elyas Hesari <elyashesari@gmail.com>
2022-08-25 11:33:01 +0430

    Mini project, Digital clock

--- _statement.js
+++ _statement.js
@@ -1,3 +1,25 @@
-function testLoad(){
-    alert("Welcome...");
+function digitClock() {
+  var d = new Date();
+  var hour = d.getHours();
+  var min = d.getMinutes();
+  var sec = d.getSeconds();
+
+  var p = "AM";
+  if (p > 12) {
+    p -= 12;
+    p = "PM";
+  }
+  if (hour < 10){
+    hour = "0" + hour;
+  }
+  if (min < 10){
+    min = "0" + min;
+  }
+  if (sec < 10){
+    sec = "0" + sec;
+  }
+  var clock = hour + ":" + min + ":" + sec + " " + p;
+  document.getElementById("testClock").textContent = clock;
+  setTimeout(digitClock, 1000);
 }
+digitClock();
